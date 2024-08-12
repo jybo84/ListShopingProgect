f59f442c598350f38248680cc9fd15879daf7fac
Maxim <jybo@mail.ru>
2024-08-12 15:02:02 +1000

    Step-5 создал LD

--- app/src/main/java/com/example/listshopingprogect/MainViewModel.kt
+++ app/src/main/java/com/example/listshopingprogect/MainViewModel.kt
@@ -1,16 +1,25 @@
 package com.example.listshopingprogect
 
+import androidx.lifecycle.MutableLiveData
 import com.example.listshopingprogect.data.ShopListRepositoryImpl
 import com.example.listshopingprogect.domain.DeleteShopItemUseCase
 import com.example.listshopingprogect.domain.EditShopItemUseCase
 import com.example.listshopingprogect.domain.GetShopListUseCase
+import com.example.listshopingprogect.domain.ShopItem
 
 class MainViewModel {
 
     private val repository = ShopListRepositoryImpl
 
-    val getShopListUseCase = GetShopListUseCase(repository)
+    private val shopList = MutableLiveData<List<ShopItem>>()
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
     val deleteShopListUseCase = DeleteShopItemUseCase(repository)
     val editShopListUseCase = EditShopItemUseCase(repository)
+
+    fun getShopItem(){
+        val element = getShopListUseCase.getShopList()
+        shopList.value = element
+    }
 
 }
