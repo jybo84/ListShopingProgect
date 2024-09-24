2f922f146a5d0256d0870a1078c7a138be382f8a
Maxim <jybo@mail.ru>
2024-09-24 13:06:22 +1000

    Step-7 добавлена база данных Room

---
+++ app/src/main/java/com/example/listshopingprogect/data/AppDatabase.kt
@@ -0,0 +1,22 @@
+package com.example.listshopingprogect.data
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ShopItemDbModel::class], version = 1)
+abstract class AppDatabase: RoomDatabase() {
+
+    abstract fun shopListDao(): ShopListDao
+
+    companion object{
+        fun getDb(application: Application): AppDatabase{
+            return Room.databaseBuilder(
+                application,
+                AppDatabase::class.java,
+                "shop_item.db"
+            ).build()
+        }
+    }
+}
